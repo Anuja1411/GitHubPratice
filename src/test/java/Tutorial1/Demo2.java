c5d81298f4234bf8b48e49d6eca317d0d2cc1717
Anuja <Anuja@DESKTOP-640JHLG>
2024-07-15 23:22:01 +0530

    Add User Test cases by Sonali Automation tester

---
+++ src/test/java/Tutorial1/Demo2.java
@@ -0,0 +1,12 @@
+package Tutorial1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Add new user Test Cases Written by Sonali Automation Tester");
+		
+		
+
+	}
+
+}
